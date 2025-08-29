01c47e6e95a3ff75bb129d4a6e0ba53a9d745d9f
Rock <miguel.garcia@fcm.digital>
2025-08-29 11:06:40 +0200

    No extra \n

--- lib/graphql_query/document.ex
+++ lib/graphql_query/document.ex
@@ -281,12 +281,11 @@ defmodule GraphqlQuery.Document do
     fragments_string =
       fragments
       |> filter_used_fragments(document)
-      |> Enum.map_join("\n\n", &Kernel.to_string/1)
+      |> Enum.map_join("\n", &Kernel.to_string/1)
 
     [String.trim(query), fragments_string]
     |> Enum.reject(&(&1 == ""))
-    |> Enum.join("\n\n")
-    |> Kernel.<>("\n")
+    |> Enum.join("\n")
   end
 
   defp filter_used_fragments(fragments, %{document_info: nil}), do: fragments
